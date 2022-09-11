cbe9e2df12f98a7cdcb521da3401bc56106019d6
susananshcutz <drsusananschutz@gmail.com>
2022-09-11 12:27:39 -0500

    working model

--- connect4.js
+++ connect4.js
@@ -9,6 +9,7 @@ const WIDTH = 7;
 const HEIGHT = 6;
 
 let currPlayer = 1; // active player: 1 or 2
+let winner = -1;
 const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
@@ -16,24 +17,23 @@ const board = []; // array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  for(let y = 0; y < HEIGHT; y++){
-    board.push(Array.from({length: WIDTH}));
-    }
+  for (let y = 0; y < HEIGHT; y++) {
+    board.push(Array.from({ length: WIDTH }));
   }
-  
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+}
 
+// TODO: set "board" to empty HEIGHT x WIDTH matrix array
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
   const board = document.getElementById("board");
   //let newTable = document.createElement("table");
-  
+
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
 
   // TODO: add comment for this code
-  // this code creates a top line not included in the gameboard to drop the tokens onto, 
+  // this code creates a top line not included in the gameboard to drop the tokens onto,
   // with an event listener
   // then appends 7 cells of table data and gives them the id of thier x
   const top = document.createElement("tr");
@@ -70,7 +70,7 @@ function findSpotForCol(x) {
     }
   }
   // TODO: write the real version of this, rather than always returning 0
-  return null;
+  return 0;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
@@ -80,11 +80,12 @@ function placeInTable(y, x) {
   //how do I give it the id of x and y
   const piece = document.createElement("div");
   piece.classList.add("piece");
-  piece.classList.add(`p${currPlayer}`);
-  piece.style.top = -50 *  (y + 2);
+  piece.classList.add(`player${currPlayer}`);
+  piece.style.top = -50 * (y + 2);
 
   const spot = document.getElementById(`${y}-${x}`);
-  spot.append(piece);
+  console.log("Should be placed here", spot);
+  spot.appendChild(piece);
 }
 
 /** endGame: announce game end */
@@ -97,31 +98,48 @@ function endGame(msg) {
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
+  if(winner > -1) {
+    return
+  }
   // get x from ID of clicked cell
   const x = +evt.target.id;
+  console.log("x", x);
 
   // get next spot in column (if none, ignore click)
   const y = findSpotForCol(x);
+  console.log(y);
   if (y === null) {
     return;
   }
-
+  console.log("y", y);
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
   board[y][x] = currPlayer;
   placeInTable(y, x);
-  
+
   // check for win
   if (checkForWin()) {
-    return endGame(`Player ${currPlayer} won!`);
+    winner = currPlayer
   }
-  if (board.every(row => row.every(cell => cell ))) {
-     return endGame('Tie!');
-      //if all cells are not null   }
+
+  if (board.every((row) => row.every((cell) => cell))) {
+    winner = 0
+    //if all cells are not null   }
   }
+
+  setTimeout(() => {
+    if(winner === 0){
+      alert("Tie!")
+    }
+    if(winner > -1) {
+      alert(`Player ${winner} wins!`)
+    }
+  }, 200)
+
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
-  currPlayer =  currPlayer === 1 ? 2 : 1;
+  
+  currPlayer = currPlayer === 1 ? 2 : 1;
   // switch players
   // TODO: switch currPlayer 1 <-> 2
 }
@@ -148,16 +166,38 @@ function checkForWin() {
 
   for (var y = 0; y < HEIGHT; y++) {
     for (var x = 0; x < WIDTH; x++) {
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+      var horiz = [
+        [y, x],
+        [y, x + 1],
+        [y, x + 2],
+        [y, x + 3],
+      ];
+      var vert = [
+        [y, x],
+        [y + 1, x],
+        [y + 2, x],
+        [y + 3, x],
+      ];
+      var diagDR = [
+        [y, x],
+        [y + 1, x + 1],
+        [y + 2, x + 2],
+        [y + 3, x + 3],
+      ];
+      var diagDL = [
+        [y, x],
+        [y + 1, x - 1],
+        [y + 2, x - 2],
+        [y + 3, x - 3],
+      ];
 
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
       }
     }
   }
+
+  return false;
 }
 
 makeBoard();
